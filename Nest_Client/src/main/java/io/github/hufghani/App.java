2a066043ebee46b952fb26c290722e274f54f757
Hamza Ghani <h.u.f.ghani@gmail.com>
2017-02-13 13:10:41 +0000

    user can now set the Temputure of the nest now

--- Nest_Client/src/main/java/io/github/hufghani/App.java
+++ Nest_Client/src/main/java/io/github/hufghani/App.java
@@ -11,18 +11,17 @@ import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
 
-public class App
-{
+public class App {
+    ObjectMapper mapper = new ObjectMapper();
     public static void main( String[] args ) {
-
         App app = new App();
-
             app.getData();
+           // app.setTemputure(15);
 
     }
 
     public void getData() {
-        ObjectMapper mapper = new ObjectMapper();
+
         try {
             Client client = Client.create();
             WebResource webResource = client
@@ -47,5 +46,27 @@ public class App
         }
     }
 
+public void setTemputure(int temputure) {
+    try {
+        Client client = Client.create();
+        WebResource webResource = client.resource("https://developer-api.nest.com/devices/thermostats/0KD654moZP0wgM2qlRxQxFcNidHWg5j2/target_temperature_c?auth=c.qOL8PQR370XkE3F2oQbSSIwBENrVMYN1DhAQJRpmxDnuzd23e2Z0Ru4fsUKgzc20pHbBkhumPXp9qgwNUJuNJXSeif7ByD2SpcHRiEoX2sNecb7SvwmK3d1X91JGwJxRxMl2fTvDa3m3aa1W");
+
+        ClientResponse response = webResource.type("text/plain").put(ClientResponse.class, String.valueOf(temputure));
+
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + response.getStatus());
+        }
+
+        System.out.println("Output from Server .... \n");
+        String output = response.getEntity(String.class);
+        System.out.println(output);
+
+    } catch (Exception e) {
+
+        e.printStackTrace();
+
+    }
+}
 
 }
