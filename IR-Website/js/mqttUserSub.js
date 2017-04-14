bc1b0e98265126a61d83ab29b132e15e9a6ab266
Glenn Lewis <glennlewis@me.com>
2017-04-14 22:50:38 +0100

    starting user preference

---
+++ IR-Website/js/mqttUserSub.js
@@ -0,0 +1,40 @@
+
+function getUser() {
+
+        client = new Paho.MQTT.Client("localhost", 1884, "clientId");
+        client.onConnectionLost = onConnectionLostUsersub;
+        client.onMessageArrived = onMessageArrivedUserSub;
+
+        // connect the client
+        client.connect({onSuccess: onConnectUsersub});
+    }
+
+    function onConnectHuesub() {
+        // Once a connection has been made, make a subscription and send a message.
+        console.log("Connect user sub");
+        client.subscribe("houseID123/userID1/preference");
+    }
+
+// called when the client loses its connection
+    function onConnectionLostUsersub(responseObject) {
+        if (responseObject.errorCode !== 0) {
+            console.log("onConnectionLost:" + responseObject.errorMessage);
+        }
+    }
+
+// called when a message arrives
+    function onMessageArrivedUserSub(message) {
+        var temp = message.payloadString;
+        suboutput = JSON.parse(temp);
+        document.getElementById("fName").innerHTML= suboutput.firstName.;
+        document.getElementById("lName").innerHTML= suboutput.lastName;
+        document.getElementById("priority").innerHTML= suboutput.priority;
+        document.getElementById("redPref").innerHTML= suboutput.lightPref.colour.red;
+        document.getElementById("greenPref").innerHTML= suboutput.lightPref.colour.green;
+        document.getElementById("bluePref").innerHTML= suboutput.lightPref.colour.blue;
+        document.getElementById("satPref").innerHTML= suboutput.lightPref.saturation;
+        document.getElementById("briPref").innerHTML= suboutput.lightPref.brightness;
+        document.getElementById("tempPref").innerHTML= suboutput.tempPref.target_temperature_c;
+        document.getElementById("tempActPref").innerHTML= suboutput.tempPref.actionMethod;
+        document.getElementById("tempActPriorityPref").innerHTML= suboutput.tempPref.actionPriority;
+}
