c65883df961409dfe2deeaa2c4c424098ebe0c84
Glenn Lewis <glennlewis@me.com>
2017-04-15 08:16:17 +0100

    publishing user preferences to the broker

---
+++ IR-Website/js/mqttUserPub.js
@@ -0,0 +1,50 @@
+
+// Create a client instance
+var json;
+function sendUser() {
+    var first     = document.getElementById("fNameEdit").value;
+    var last   = document.getElementById("lNameEdit").value;
+    var prio    = document.getElementById("priorityEdit").value;
+    var red     = document.getElementById("redPrefEdit").value;
+    var green     = document.getElementById("greenPrefEdit").value;
+    var blue    = document.getElementById("bluePrefEdit").value;
+    var satur     = document.getElementById("satPrefEdit").value;
+    var bright   = document.getElementById("briPrefEdit").value;
+    var lact    = document.getElementById("lightActPrefEdit").value;
+    var lactp     = document.getElementById("lightActPriorityPrefEdit").value;
+    var temp     = document.getElementById("tempPrefEdit").value;
+    var tact    = document.getElementById("tempActPrefEdit").value;
+    var tactp     = document.getElementById("tempActPriorityPrefEdit").value;
+    
+    
+    json = "{\"userId\":\"user1\",";
+    json+= "\"firstName\":" + first +",";
+    json+= "\"lastName\":" + last +",";
+    json+= "\"priority\":" + prio +",";
+    json+= "\"lightPref\":{" ;
+    json+= "\"colour\":{" ;
+    json+= "\"red\":"+ red +",";
+    json+= "\"green\":" + green + ",";
+    json+= "\"blue\":" + blue +"},";
+    json+= "\"saturation\":" + satur +",";
+    json+= "\"brightness\":" + bright +",";
+    json+= "\"actionMethod\":" + lact +",";
+    json+= "\"actionPriority\":" + lactp +"},";
+    json+= "\"tempPref\":{" ;
+    json+= "\"target_temperature_c\":" + temp +",";
+    json+= "\"actionMethod\":" + tact +",";
+    json+= "\"actionPriority\":" + tactp +"}}";
+    
+    client = new Paho.MQTT.Client("localhost", 1884, "clientId1");
+// connect the client
+    client.connect({onSuccess: onConnectUserPub});
+}
+
+// called when the client connects
+function onConnectUserPub() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect user pub");
+    message = new Paho.MQTT.Message(json);
+    message.destinationName = "houseID123/user1/preference";
+    client.send(message);
+}
--- IR-Website/js/script.js
+++ IR-Website/js/script.js
@@ -17,3 +17,43 @@ $("#tempNew").slider();
 $("#tempNew").on("slide", function(slideEvt) {
 	$("#tempNewVal").text(slideEvt.value);
 });
+
+
+// With JQuery
+$("#redPrefEdit").slider();
+$("#redPrefEdit").on("slide", function(slideEvt) {
+	$("#briNewVal").text(slideEvt.value);
+});
+
+// With JQuery
+$("#greenPrefEdit").slider();
+$("#greenPrefEdit").on("slide", function(slideEvt) {
+	$("#satNewVal").text(slideEvt.value);
+});
+
+
+// With JQuery
+$("#bluePrefEdit").slider();
+$("#bluePrefEdit").on("slide", function(slideEvt) {
+	$("#tempNewVal").text(slideEvt.value);
+});
+
+
+// With JQuery
+$("#satPrefEdit").slider();
+$("#briNew").on("slide", function(slideEvt) {
+	$("#briNewVal").text(slideEvt.value);
+});
+
+// With JQuery
+$("#briPrefEdit").slider();
+$("#satNew").on("slide", function(slideEvt) {
+	$("#satNewVal").text(slideEvt.value);
+});
+
+
+// With JQuery
+$("#tempPrefEdit").slider();
+$("#tempNew").on("slide", function(slideEvt) {
+	$("#tempNewVal").text(slideEvt.value);
+});
