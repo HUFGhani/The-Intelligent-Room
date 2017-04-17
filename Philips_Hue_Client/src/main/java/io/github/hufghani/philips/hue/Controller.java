014336cd5ba386bef83a4d9dd12746dd2bd36421
Hamza Ghani <h.u.f.ghani@gmail.com>
2017-04-17 15:50:03 +0100

    fix

--- Philips_Hue_Client/src/main/java/io/github/hufghani/philips/hue/Controller.java
+++ Philips_Hue_Client/src/main/java/io/github/hufghani/philips/hue/Controller.java
@@ -186,7 +186,7 @@ public class Controller implements PHSDKListener {
         PHLightState lightState = new PHLightState();
 //        int r = 0,g = 0,b = 0,br = 0, sa = 0;
 
-        if (r != red && g != green && b != blue || br != bri || sa != sat && isOnOff != isOnorOff) {
+        if (r != red && g != green && b != blue || br != bri || sa != sat ) {
             if (checkRedValue(red) &&
                     checkGreenValue(green) &&
                     checkBlueValue(blue) &&
