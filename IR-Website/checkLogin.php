b43f3571119af06b526a988f88bf7159ee0a573b
Glenn Lewis <glennlewis@me.com>
2017-04-20 23:30:44 +0100

    added a back button, in the event of invalid login details being entered to return to login screen

--- IR-Website/checkLogin.php
+++ IR-Website/checkLogin.php
@@ -33,16 +33,31 @@ while ($row = $result->fetch_assoc()) {
     $_SESSION['Email'] = $row["Email"];
     $_SESSION['UserID'] = $row["UserID"];
     
-    if ($row >0){
+    if ($row = 1){
         header('Location: index.php');  
     } 
+    else {
+        
+    }
 }
 
-echo $Email;
-echo $UserPassword;
+echo "Incorrect Login Details, please go back and try again...";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function goBack() {
+    window.history.back()
+}
+</script>
+</head>
+<body>
+<br>
+<button onclick="goBack()">Go Back</button>
+</body>
+</html>
 
-
-
+<?
 $stmt->close();
-
 ?>
