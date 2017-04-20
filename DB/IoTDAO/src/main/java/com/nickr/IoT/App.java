4fdf0164f24209709c188133945637d94e24f100
Shnoberts91 <nickroberts1991@hotmail.co.uk>
2017-04-20 10:30:28 +0100

    MQTT connection

--- DB/IoTDAO/src/main/java/com/nickr/IoT/App.java
+++ DB/IoTDAO/src/main/java/com/nickr/IoT/App.java
@@ -15,31 +15,31 @@ public class App {
 
     public App() {
 
-//        HueMQTT hueMQTT = new HueMQTT();
-//        HueMQTTStatus hueMQTTStatus =  new HueMQTTStatus();
-//        NestMQTT nestMQTT = new NestMQTT();
-//        NestMQTTStatus nestMQTTStatus = new NestMQTTStatus();
-//        SensorMQTT sensorMQTT = new SensorMQTT();
-//        HouseMQTT houseMQTT = new HouseMQTT();
-//        PreferenceMQTT preferenceMQTT = new PreferenceMQTT();
-//        ConfigurationMQTT configurationMQTT = new ConfigurationMQTT();
-//
-//        for (;;) {
-//            try {
-//                hueMQTT.subscribe();
-//                hueMQTTStatus.subscribe();
-//                nestMQTT.subscribe();
-//                nestMQTTStatus.subscribe();
-//                sensorMQTT.subscribe();
-//                houseMQTT.subscribe();
-//                preferenceMQTT.subscribe();
-//                configurationMQTT.subscribe();
-//
-//                Thread.sleep(1000);
-//            }catch (Exception e){
-//                e.printStackTrace();
-//            }
-//        }
+        HueMQTT hueMQTT = new HueMQTT();
+        HueMQTTStatus hueMQTTStatus =  new HueMQTTStatus();
+        NestMQTT nestMQTT = new NestMQTT();
+        NestMQTTStatus nestMQTTStatus = new NestMQTTStatus();
+        SensorMQTT sensorMQTT = new SensorMQTT();
+        HouseMQTT houseMQTT = new HouseMQTT();
+        PreferenceMQTT preferenceMQTT = new PreferenceMQTT();
+        ConfigurationMQTT configurationMQTT = new ConfigurationMQTT();
+
+        for (;;) {
+            try {
+                hueMQTT.subscribe();
+                hueMQTTStatus.subscribe();
+                nestMQTT.subscribe();
+                nestMQTTStatus.subscribe();
+                sensorMQTT.subscribe();
+                houseMQTT.subscribe();
+                preferenceMQTT.subscribe();
+                configurationMQTT.subscribe();
+
+                Thread.sleep(1000);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
 
 
 // TEST STATEMENTS
