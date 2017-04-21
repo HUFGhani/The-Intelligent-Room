5b09a705173af0c7fb2440d4af90f4f6f3101ffb
Glenn Lewis <glennlewis@me.com>
2017-04-21 21:54:52 +0100

    mudfoot credentials added

--- IR-Website/Connection.php
+++ IR-Website/Connection.php
@@ -1,10 +1,10 @@
 <?php
 
 // 1. Create a database connection
-$dbhost = "ec2-52-56-203-226.eu-west-2.compute.amazonaws.com";
-$dbuser = "root";
-$dbpass = "TwN20nDwuJGB";
-$dbname = "iot";
+$dbhost = "mudfoot.doc.stu.mmu.ac.uk";
+$dbuser = "robertsn";
+$dbpass = "ensteLas6";
+$dbname = "robertsn";
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname,3306);
 
